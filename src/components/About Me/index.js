bbc835eaf52aa24d583faf70917f1ea7858fe7f3
troat9824 <troat9824@gmail.com>
2022-05-23 05:07:18 -0500

    initial

---
+++ src/components/About Me/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className='nav-bar'>
+            <h2 id="about">About Me</h2>
+            <h2 id="work-ive-done">Work I've Done</h2>
+            <h2 id="contact-info">Contact Information</h2>
+        </section>
+    )
+}
+
+export default About;
---
+++ src/components/Nav bar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <header>
+            <h1>
+                <a href="/">
+                    Taylor Vaughn
+                </a>
+            </h1>
+        </header>
+    )
+}
+
+export default Nav;
